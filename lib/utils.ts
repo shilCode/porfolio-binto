45afe91b6d7a7c4a20be9b6e848806fc04093800
shilcode <2850542@gmail.com>
2026-07-03 17:17:30 +0200

    updated utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,8 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  const processEnv = process.env;
+  processEnv.NODE_ENV = process.env.NODE_ENV ?? "production";
   return twMerge(clsx(inputs))
+  
 }
